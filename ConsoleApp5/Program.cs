bf985b498feebc5d37d7fcef64db9a0677106d88
Артём <@CARL>
2023-05-14 15:14:19 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            for (a=0; a<100; a++)
+            {
+                if (a % 3 == 0 && a % 5 == 0)
+                {
+                    Console.Write("FizzBuzz, ");
+                }
+                else if (a % 3 == 0)
+                {
+                    Console.Write("Fizz, ");
+                }
+                else if (a % 5 == 0)
+                {
+                    Console.Write("Buzz, ");
+                }
+                else
+                {
+                    Console.Write(a + ", ");
+                }
+            }
+        }
+    }
+}
